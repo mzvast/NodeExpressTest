d3f1e4cbccd37f5eefbbf6c1ca1a56c1c2a2dde8
mzvast <mzvast@gmail.com>
2015-12-12 13:59:11 +0000

    ADD partials and section

--- meadowlark.js
+++ meadowlark.js
@@ -4,7 +4,14 @@ var app = express();
 /*handlebars模板引擎配置*/
 var handlebars = require('express3-handlebars')
     .create({
-        defaultLayout: 'main'
+        defaultLayout: 'main',
+        helpers: {
+            section: function(name, options) {
+                if (!this._sections) this._sections = {};
+                this._sections[name] = options.fn(this);
+                return null;
+            }
+        }
     });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -15,9 +22,43 @@ app.set('port', process.env.PORT); // || 3000);
 /*静态文件目录*/
 app.use(express.static(__dirname + '/public'));
 
+// mocked weather data
+function getWeatherData() {
+    return {
+        locations: [{
+            name: 'Portland',
+            forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+            iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+            weather: 'Overcast',
+            temp: '54.1 F (12.3 C)',
+        }, {
+            name: 'Bend',
+            forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+            iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+            weather: 'Partly Cloudy',
+            temp: '55.0 F (12.8 C)',
+        }, {
+            name: 'Manzanita',
+            forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+            iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+            weather: 'Light Rain',
+            temp: '55.0 F (12.8 C)',
+        }, ],
+    };
+}
+
+// middleware to add weather data to context
+app.use(function(req, res, next) {
+    if (!res.locals.partials) res.locals.partials = {};
+    res.locals.partials.weatherContext = getWeatherData();
+    next();
+});
 /*路由*/
 app.get('/', function(req, res) {
     res.render('home');
+});
+app.get('/jquery', function(req, res) {
+    res.render('jquery-test');
 });
 app.get('/about', function(req, res) {
     var fortunes = [
@@ -33,7 +74,6 @@ app.get('/about', function(req, res) {
         fortune: randomFortune
     });
 });
-
 // 404 catch-all handler (middleware)
 app.use(function(req, res, next) {
     res.status(404);
