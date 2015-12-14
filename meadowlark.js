c1415a549206535a0e776e454f4cb064d4a50425
mzvast <mzvast@gmail.com>
2015-12-14 13:46:18 +0000

    FIX testing ERROR

--- meadowlark.js
+++ meadowlark.js
@@ -1,141 +1,66 @@
 var express = require('express');
+var fortune = require('./lib/fortune.js');
+
 var app = express();
 
-/*handlebars模板引擎配置*/
-var handlebars = require('express3-handlebars')
-    .create({
-        defaultLayout: 'main',
-        helpers: {
-            section: function(name, options) {
-                if (!this._sections) this._sections = {};
-                this._sections[name] = options.fn(this);
-                return null;
-            }
+// set up handlebars view engine
+var handlebars = require('express3-handlebars').create({
+    defaultLayout:'main',
+    helpers: {
+        section: function(name, options){
+            if(!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
         }
-    });
+    }
+});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
-/*监听端口*/
-app.set('port', process.env.PORT); // || 3000);
+app.set('port', process.env.PORT || 3000);
 
-/*静态文件目录*/
 app.use(express.static(__dirname + '/public'));
 
-// mocked weather data
-function getWeatherData() {
-    return {
-        locations: [{
-            name: 'Portland',
-            forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
-            iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
-            weather: 'Overcast',
-            temp: '54.1 F (12.3 C)',
-        }, {
-            name: 'Bend',
-            forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
-            iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
-            weather: 'Partly Cloudy',
-            temp: '55.0 F (12.8 C)',
-        }, {
-            name: 'Manzanita',
-            forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
-            iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
-            weather: 'Light Rain',
-            temp: '55.0 F (12.8 C)',
-        }, ],
-    };
-}
-/*中间件*/
-//For Page Testing
-app.use(function(req, res, next) {
-    res.locals.showTests = app.get('env') !== 'production' &&
-        req.query.test === '1';
-    next();
+// set 'showTests' context property if the querystring contains test=1
+app.use(function(req, res, next){
+	res.locals.showTests = app.get('env') !== 'production' && 
+		req.query.test === '1';
+	next();
 });
-// middleware to add weather data to context
-app.use(function(req, res, next) {
-    if (!res.locals.partials) res.locals.partials = {};
-    res.locals.partials.weatherContext = getWeatherData();
-    next();
+
+app.get('/', function(req, res) {
+	res.render('home');
 });
-app.use(require('body-parser')());
-/*路由*/
-app.get('/tours/hood-river', function(req, res) {
-    res.render('tours/hood-river');
+app.get('/about', function(req,res){
+	res.render('about', { 
+		fortune: fortune.getFortune(),
+		pageTestScript: '/qa/tests-about.js' 
+	} );
+});
+app.get('/tours/hood-river', function(req, res){
+	res.render('tours/hood-river');
 });
 app.get('/tours/oregon-coast', function(req, res){
 	res.render('tours/oregon-coast');
 });
-app.get('/tours/request-group-rate', function(req, res) {
-    res.render('tours/request-group-rate');
-});
-app.get('/', function(req, res) {
-    res.render('home');
-});
-app.get('/jquery', function(req, res) {
-    res.render('jquery-test');
-});
-app.get('/about', function(req, res) {
-    var fortunes = [
-        "Conquer your fears or they will conquer you.",
-        "Rivers need springs.",
-        "Do not fear what you don't know.",
-        "You will have a pleasant surprise.",
-        "Whenever possible, keep it simple.",
-    ];
-    var randomFortune =
-        fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', {
-        fortune: randomFortune,
-        pageTestScript: '/qa/tests-about.js'
-    });
-});
-
-app.get('/nursery-rhyme', function(req, res) {
-    res.render('nursery-rhyme');
-});
-app.get('/data/nursery-rhyme', function(req, res) {
-    res.json({
-        animal: 'squirrel',
-        bodyPart: 'tail',
-        adjective: 'bushy',
-        noun: 'heck',
-    });
-});
-app.get('/newsletter', function(req, res) {
-    // we will learn about CSRF later...for now, we just
-    // provide a dummy value
-    res.render('newsletter', {
-        csrf: 'CSRF token goes here'
-    });
-});
-app.post('/process', function(req, res) {
-    if (req.xhr || req.accepts('json,html') === 'json') {
-        // if there were an error, we would send { error: 'error description' }
-        res.send({
-            success: true
-        });
-    }
-    else {
-        // if there were an error, we would redirect to an error page
-        res.redirect(303, '/thank-you');
-    }
+app.get('/tours/request-group-rate', function(req, res){
+	res.render('tours/request-group-rate');
 });
 
 // 404 catch-all handler (middleware)
-app.use(function(req, res, next) {
-    res.status(404);
-    res.render('404');
-});
-// 500 error handler (middleware)
-app.use(function(err, req, res, next) {
-    console.error(err.stack);
-    res.status(500);
-    res.render('500');
+app.use(function(req, res, next){
+	res.status(404);
+	res.render('404');
 });
 
-app.listen(app.get('port'), function() {
-    console.log('Express started on http://localhost:' +
-        app.get('port') + '; press Ctrl-C to terminate.');
+// 500 error handler (middleware)
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log( 'Express started on http://localhost:' + 
+    app.get('port') + '; press Ctrl-C to terminate.' );
 });
